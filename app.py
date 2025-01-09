d51d93ef239728eef652460b9009170a44407ef3
mishagordeev <onitself@gmail.com>
2025-01-09 12:58:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import json
 import firebase_admin
 from firebase_admin import credentials
 from io import StringIO
-from flask import Flask
+from flask import Flask, jsonify, request
 import logging
+from google.cloud import firestore
 
 # Получаем строку JSON из переменной окружения
 json_key_str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
@@ -21,6 +22,7 @@ if json_key_str:
         # Загружаем учетные данные из виртуального файла
         cred = credentials.Certificate(json.load(json_file))
         firebase_admin.initialize_app(cred)
+        db = firestore.Client.from_service_account_json(json_file)
         print("Firebase app initialized successfully.")
     except json.JSONDecodeError:
         print("Error decoding JSON from the GOOGLE_APPLICATION_CREDENTIALS environment variable.")
@@ -32,32 +34,28 @@ else:
 # Инициализация Flask приложения
 app = Flask(__name__)
 
-# # Коллекция, где будут храниться данные
-# COLLECTION_NAME = "character_data"
+# Коллекция, где будут храниться данные
+COLLECTION_NAME = "character_data"
 
-# @app.route('/')
-# def character_sheet():
-#     doc_ref = db.collection(COLLECTION_NAME).document('main')
-#     doc = doc_ref.get()
-#     if doc.exists:
-#         character_data = doc.to_dict()
-#         return jsonify(character_data)
-#     else:
-#         return jsonify({"error": "No character data found."}), 404
+@app.route('/')
+def character_sheet():
+    doc_ref = db.collection(COLLECTION_NAME).document('main')
+    doc = doc_ref.get()
+    if doc.exists:
+        character_data = doc.to_dict()
+        return jsonify(character_data)
+    else:
+        return jsonify({"error": "No character data found."}), 404
 
-# @app.route('/update', methods=['POST'])
-# def update_character_sheet():
-#     try:
-#         character_data = request.json
-#         doc_ref = db.collection(COLLECTION_NAME).document('main')
-#         doc_ref.set(character_data)
-#         return jsonify({"status": "success"})
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/update', methods=['POST'])
+def update_character_sheet():
+    try:
+        character_data = request.json
+        doc_ref = db.collection(COLLECTION_NAME).document('main')
+        doc_ref.set(character_data)
+        return jsonify({"status": "success"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/test")
 def test():
